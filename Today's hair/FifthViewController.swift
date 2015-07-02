7ede526a511c8e7dae6af6b8cec58f0a2ea25dd7
tamura-seiya <s262113@gmail.com>
2015-07-03 04:30:44 +0900

    Twitter Button

--- Today's hair/FifthViewController.swift
+++ Today's hair/FifthViewController.swift
@@ -48,6 +48,8 @@ class FifthViewController: UIViewController, UINavigationControllerDelegate, UII
     let ResultPic27: UIImage! = UIImage(named: "27.jpeg")
     let ResultPic28: UIImage! = UIImage(named: "19.jpg")
 
+    var myComposeView : SLComposeViewController!
+    var myTwitterButton: UIButton!
     
 
     
@@ -111,10 +113,70 @@ class FifthViewController: UIViewController, UINavigationControllerDelegate, UII
         }else if(operation == 8 && operation2 == 3){
             lastImage?.image = ResultPic27
         }
+        
+        // Twitter用のUIButtonの生成.
+        let hex: Int = 0x55ACEE
+        let red = Double((hex & 0xFF0000) >> 16) / 255.0
+        let green = Double((hex & 0xFF00) >> 8) / 255.0
+        let blue = Double((hex & 0xFF)) / 255.0
+        var myColor: UIColor = UIColor( red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(1.0))
+        
+        // ボタン.
+        myTwitterButton = UIButton()
+        myTwitterButton.frame = CGRectMake(0,0,100,100)
+        myTwitterButton.backgroundColor = myColor
+        myTwitterButton.layer.masksToBounds = true
+        myTwitterButton.setTitle("Twitter", forState: UIControlState.Normal)
+        myTwitterButton.titleLabel?.font = UIFont.systemFontOfSize(CGFloat(20))
+        myTwitterButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        myTwitterButton.layer.cornerRadius = 50.0
+        myTwitterButton.layer.position = CGPoint(x: self.view.frame.width/4.1
+            , y:self.view.frame.height/1.25)
+        myTwitterButton.tag = 1
+        myTwitterButton.addTarget(self, action: "onPostToTwitter:", forControlEvents: .TouchUpInside)
+        
+        // buttonをviewに追加.
+        self.view.addSubview(myTwitterButton)
+        
+        
+        
+        
+    }
+    
+    // ボタンイベント.
+    func onPostToTwitter(sender : AnyObject) {
+        
+        // SLComposeViewControllerのインスタンス化.
+        // ServiceTypeをTwitterに指定.
+        myComposeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        
+        // 投稿するテキストを指定.
+        myComposeView.setInitialText("Today's Hair最高！もう本当に毎日使いたい！！！")
+        
+        // 投稿する画像を指定.
+        myComposeView.addImage(UIImage(named: "oouchi.jpg"))
+        
+        // myComposeViewの画面遷移.
+        self.presentViewController(myComposeView, animated: true, completion: nil)
+        
+           }
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+
 
         
             
-}
+
         
         
         
@@ -131,12 +193,7 @@ class FifthViewController: UIViewController, UINavigationControllerDelegate, UII
 
     
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        
-    }
-
+  /*
     @IBAction func twitter(sender: AnyObject) {
     // availability check
     if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
@@ -156,7 +213,5 @@ class FifthViewController: UIViewController, UINavigationControllerDelegate, UII
         presentViewController(controller, animated: true, completion: {})
     }
 }
+*/
 
-
-
-    }
